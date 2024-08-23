f324749687e93311dbb7cfdcae7cd5d4f136e5f7
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-08-23 14:47:08 +0200

    added basic frontend code for rendering books

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
-export default function Home() {
+export default async function Home() {
+
+  const books = await fetch("http://localhost:3000/api/books/")
+    .then(response => response.json())
+    .catch((error) => {
+      console.log("failed to get books", error)
+    })
+
+  console.log("BOOKS IN FRONTEND", books)
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       BACKEND
+      {books && books.results.map(book => (
+        <pre>
+          {JSON.stringify(book,null,2)}
+        </pre>
+      ))}
     </main> 
   );
 }
