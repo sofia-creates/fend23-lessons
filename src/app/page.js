6b47ba3e9849183365b01dd1d8f01be07f95cf2d
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-08-29 09:11:50 +0200

    styling

--- src/app/page.js
+++ src/app/page.js
@@ -10,12 +10,34 @@ export default async function Home() {
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      BACKEND
-      {books && books.results.map(book => (
-        <pre>
-          {JSON.stringify(book,null,2)}
-        </pre>
-      ))}
+      Books
+      <section className="flex flex-col items-center justify-center gap-2">
+        {books && books.results.map(book => (
+          // <pre>
+          //   {JSON.stringify(book,null,2)}
+          // </pre>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4">
+          <div className="font-bold text-xl mb-2">{book.title}</div>
+          <p className="text-gray-700 text-base">
+            <span className="font-semibold">Author:</span> {book.author}
+          </p>
+          <p className="text-gray-700 text-base">
+            <span className="font-semibold">Year:</span> {book.year}
+          </p>
+          <p className="text-gray-700 text-base">
+            <span className="font-semibold">Genre:</span> {book.genre}
+          </p>
+          <div className="mt-4">
+            <p className="text-gray-700 font-semibold">Keywords:</p>
+            <ul className="list-disc list-inside">
+              {book.keywords.map((keyword, index) => (
+                <li key={index} className="text-gray-600">{keyword}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        ))}
+      </section>
     </main> 
   );
 }
