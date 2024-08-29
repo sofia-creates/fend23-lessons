9ababf1290c07f11ed18bec993e87cabfea3a883
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-08-29 15:53:49 +0200

    added basic prisma for project

--- src/app/api/books/route.js
+++ src/app/api/books/route.js
@@ -2,59 +2,58 @@ import { NextResponse } from "next/server";
 
 import { lowerCaseCompare, validateBookData } from "@/utils/helpers/apiHelpers";
 
-import books from "@/data/books";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET(req) {
   const url = new URL(req.url);
   const search = url.searchParams.get("search");
-
-  let filteredBooks = [...books]; //Simulates databse callss
-
+  let books = [];
   if (search) {
-    filteredBooks = filteredBooks.filter(
-      (book) =>
-        lowerCaseCompare(book.title, search) ||
-        lowerCaseCompare(book.author, search) ||
-        book.keywords.some(keyword => {
-            return lowerCaseCompare(keyword, search)
-        })
-    );
+    books = await prisma.book.findMany({
+        where: {
+            title: {
+                contains: search,
+                mode: 'insensitive',
+            }
+        }
+    })
+  } else {
+    books = await prisma.book.findMany();
   }
 
-  return NextResponse.json({ results: filteredBooks });
+  return NextResponse.json(books);
 }
 
 export async function POST(req) {
-    let body;
-    try {
-        body = await req.json()
-    }
-    catch(error) {
-        return NextResponse.json({
-            message: "A valid JSON object has to be sent"
-        },{
-            status: 400
-        })
-    }
-    const [hasErrors, errors] = validateBookData(body)
-    if(hasErrors) {
-        return NextResponse.json({
-            errors
-        }, {
-            status: 400
-        })
-    }
+  let body;
+  try {
+    body = await req.json();
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "A valid JSON object has to be sent",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  // const [hasErrors, errors] = validateBookData(body)
+  // if(hasErrors) {
+  //     return NextResponse.json({
+  //         errors
+  //     }, {
+  //         status: 400
+  //     })
+  // }
 
-    let filteredBooks = [...books]; //Simulates databse callss
-    
-    const book = {
-        id: filteredBooks.length+1,
-        ...body
-    }
+  const newBook = await prisma.book.create({
+    data: body,
+  });
 
-    books.push(book)
-    
-    return NextResponse.json(book, {
-        status: 201
-    })
+  return NextResponse.json(newBook, {
+    status: 201,
+  });
 }
